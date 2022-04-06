944ef8fecc4e1e3a0dafc8709d7e933cec0c87e9
Hanwe Lee <mobiledev@kakao.com>
2022-04-06 21:29:13 +0900

    init

---
+++ CapturePreventionKitSwift/CapturePreventionKitSwift/Module/CVPixelBufferSupport.swift
@@ -0,0 +1,14 @@
+//
+//  CVPixelBufferSupport.swift
+//  CapturePreventionKitSwift
+//
+//  Created by hanwe on 2022/04/06.
+//
+
+import CoreVideo
+
+class CVPixelBufferSupport: NSObject {
+    func test() -> CVReturn {
+        
+    }
+}
---
+++ CapturePreventionKitSwift/CapturePreventionKitSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CapturePreventionKitSwift
+//
+//  Created by hanwe on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var testLabel1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.testLabel1.text = "hello world"
+    }
+
+
+}
+
